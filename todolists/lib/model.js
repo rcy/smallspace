a6eea679d315aafe708c812c6cc25942e6943fa1
Ryan Yeske <rcyeske@gmail.com>
2013-05-03 22:09:30 -0700

    remove logging

--- todolists/lib/model.js
+++ todolists/lib/model.js
@@ -8,7 +8,6 @@ if (Meteor.isServer) {
 
   Meteor.publish('list-elements', function(spaceId, listId) {
     if (spaceId && listId) {
-      console.log('list-elements publish', spaceId, listId);
       return ListElements.find( { spaceId: spaceId,
                                   listId: listId },
                                 { sort: { created: -1 } } );
@@ -38,7 +37,6 @@ if (Meteor.isServer) {
 
 Meteor.methods({
   trash: function(collectionName, id) {
-    console.log('trash', id)
     var set = { $set: { trash: true, updated: Date.now() } };
 
     // XXX How to map from text collection name to actual object.  Maybe extend Meteor.collection
