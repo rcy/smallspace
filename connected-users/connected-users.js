aefbf50a52ba31edc3c60180e1271c8f58fb4541
Ryan Yeske <rcyeske@gmail.com>
2013-05-12 08:16:07 -0700

    don't show global presence all the time

--- connected-users/connected-users.js
+++ connected-users/connected-users.js
@@ -3,13 +3,15 @@
 // keeps track of connected users, and publishes to client so that
 // presence information can be shared
 
+// https://github.com/tmeasday/meteor-presence
+
 if (Meteor.isServer) {
-  Meteor.publish('userPresence', function() {
+  Meteor.publish('userPresence', function(spaceId) {
     console.log('publish userPresence');
     // Setup some filter to find the users your logged in user
     // cares about. It's unlikely that you want to publish the
     // presences of _all_ the users in the system.
-    var filter = {};
+    var filter = {"state.currentSpace": spaceId};
 
     // ProTip: unless you need it, don't send lastSeen down as it'll make your
     // templates constantly re-render (and use bandwidth)
@@ -19,7 +21,9 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
   Meteor.startup( function() {
-    Meteor.subscribe('userPresence');
+    Deps.autorun(function() {
+      Meteor.subscribe('userPresence', Session.get('currentSpace'));
+    });
 
     Meteor.presences.find().observe({
       added: function(cu) {
@@ -30,6 +34,13 @@ if (Meteor.isClient) {
       }
     });
   });
+
+  Meteor.Presence.state = function() {
+    return {
+      online: true,
+      currentSpace: Session.get('currentSpace')
+    }
+  }
 
   Template.connected.helpers({
     onlineUsers: function() {
